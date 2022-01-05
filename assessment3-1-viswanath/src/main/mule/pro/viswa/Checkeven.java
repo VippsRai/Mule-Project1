4f1804ec2087acbc24ad9b9c76009e9feb956445
Viswanathrajula <rviswaanath@gmail.com>
2022-01-05 14:09:02 +0530

    assesment3

---
+++ assessment3-1-viswanath/src/main/mule/pro/viswa/Checkeven.java
@@ -0,0 +1,13 @@
+package pro.viswa;
+
+public class Checkeven {
+	public void evenOrNumberisnotvalid(int number) {
+		  if (number % 2 == 0) {
+		    System.out.println("Number is Even");
+		   } else {
+		    System.out.println("Number is not Valid ");
+		  }
+		 }
+	}
+
+
