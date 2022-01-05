d8fbce085f92c49cbf3787237226d538eb6b29cc
Viswanathrajula <rviswaanath@gmail.com>
2022-01-05 14:32:43 +0530

    java module

---
+++ java-module/src/main/java/com/porj/samplejavamodule.java
@@ -0,0 +1,35 @@
+package com.porj;
+
+public class samplejavamodule {
+
+		public static String welcome(String name) {
+
+		return "Welcome to MuleSoft "+ name;
+
+		}
+
+
+		public String getBalance(String bankName) {
+
+
+
+		if(bankName.equals("HDFC")) {
+
+		return "1000";
+
+		}
+
+		else if(bankName.equals("SBI")) {
+
+		return "2000";
+
+		}
+
+		else {
+
+		return "Bank doesn't exist";
+
+		}
+
+}
+}
